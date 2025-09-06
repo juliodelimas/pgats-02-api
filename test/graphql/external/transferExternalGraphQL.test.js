213bb1fa12bb37432afb6734e014a88f13a40b41
criscandeloro <criscandeloro@gmail.com>
2025-09-06 15:55:36 -0300

    Adicionando testes em GraphQL

---
+++ test/graphql/external/transferExternalGraphQL.test.js
@@ -0,0 +1,50 @@
+// Bibliotecas
+const request = require('supertest');
+const { expect, use } = require('chai');
+
+const chaiExclude = require('chai-exclude');
+use(chaiExclude)
+
+// Testes
+describe('Testes de Transferência', () => {
+
+    before(async () => {
+         const loginUser = require('../fixture/requisicoes/login/loginUser.json');
+         const resposta = await request('http://localhost:4000/graphql')
+            .post('')
+            .send(loginUser);
+           token = resposta.body.data.loginUser.token;
+        });
+
+    beforeEach(() => {
+        createTransfer = require('../fixture/requisicoes/transferencia/createTransfer.json');
+    });   
+
+
+
+    it('Validar que é possível transferir grana entre duas contas', async () => {
+        //Escrever o teste
+            const respostaEsperada = require('../fixture/respostas/transferencia/validarQueEPossivelTransferirGranaEntreDuasContas.json');
+
+            const respostaTransferencia = await request('http://localhost:4000/graphql')
+            .post('')
+            .set('Authorization', `Bearer ${token}`)
+            .send(createTransfer);
+            expect(respostaTransferencia.status).to.equal(200);
+            expect(respostaTransferencia.body.data.createTransfer)
+                   .excluding('date')
+                   .to.deep.equal(respostaEsperada.data.createTransfer);
+        })
+
+        it('Validar que não é possível transferir quando o saldo é insuficiente', async () => {
+        //Escrever o teste  
+         createTransfer.variables.value = 10000.01
+         const respostaSaldoInsuficiente = await request('http://localhost:4000/graphql')
+         
+            .post('')
+            .set('Authorization', `Bearer ${token}`)
+            .send(createTransfer);
+            expect(respostaSaldoInsuficiente.status).to.equal(200);
+            expect(respostaSaldoInsuficiente.body.errors[0].message).to.equal('Saldo insuficiente');
+        })
+    });
